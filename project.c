fe3c0835c61d813b51d25ac8e7c8950ece80b6a4
JeetJani-11 <121782735+JeetJani-11@users.noreply.github.com>
2023-01-02 10:37:49 +0530

    Day 1

---
+++ project.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main () {
+    FILE *fptr ;
+    fptr = fopen("Data.txt", "a");
+    fprintf(fptr, "%c", 'M');
+    fprintf(fptr, "%c", 'A');
+    fprintf(fptr, "%c", 'N');
+    fprintf(fptr, "%c", 'G');
+    fprintf(fptr, "%c", 'O');
+    fclose(fptr);
+    return 0 ;
+}
